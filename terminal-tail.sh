ed24415a830c785c0fa65075aa114ab0efb65dc5
Drahflow <drahflow@gmx.de>
2016-08-28 09:40:57 +0200

    Arbitrary file names

--- terminal-tail.sh
+++ terminal-tail.sh
@@ -1,1 +1,3 @@
-echo -ne '\e[2J\e[?25l'; while sleep 0.05; do echo -ne '\e[1;1H'; tail -c 1024 2016-08-27 | w3m -cols 60 -T text/html -dump | tail -n 16 | sed -e 's/^/[2K/g'; done
+#!/bin/sh
+
+echo -ne '\e[2J\e[?25l'; while sleep 0.05; do echo -ne '\e[1;1H'; tail -c 1024 "$1" | w3m -cols 60 -T text/html -dump | tail -n 16 | sed -e 's/^/[2K/g'; done
